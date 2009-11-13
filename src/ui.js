a000bd66f811b7445eb059db7120747e75023b72
Andrew Dupont <prototype@andrewdupont.net>
2009-11-12 19:42:00 -0600

    Add PDoc section names where needed.

--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,10 @@
+/**
+ *  == scripty2 UI ==
+ *
+ *  A set of flexible, extensible, skinnable UI components not provided by
+ *  vanilla HTML.
+**/
+
 // TODO: Remove this when Thomas renames the root object.
 if (!window.S2) {
   S2 = s2;
@@ -5,7 +12,7 @@ if (!window.S2) {
 
 //= require "layout"
 
-/**
+/** section: scripty2 UI
  *  S2.UI
 **/
 
